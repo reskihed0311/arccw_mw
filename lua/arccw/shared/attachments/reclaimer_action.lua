838950941e7e26b72978267007cd4cf3a1c4d93d
NotTrashraD <bellie1579@gmail.com>
2024-10-19 10:57:30 +0200

    i cant sleep (spas12 / reclaimer18)

---
+++ lua/arccw/shared/attachments/reclaimer_action.lua
@@ -0,0 +1,30 @@
+att.PrintName = "Manual Action"
+
+
+att.Description = "Pump yo shit manually."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.Desc_Neutrals = {
+}
+att.Slot = "reclaimer_fcg"
+att.AutoStats = true
+
+att.SortOrder = -5
+
+
+att.Override_Firemodes = {
+    {
+        Mode = 1,
+        PrintName = "Semi-Auto"
+    },
+    {
+        Mode = 0
+    }
+}
+
+att.Override_ManualAction = true
+att.Mult_AccuracyMOA = 0.5
+att.Mult_Range = 1.35
+att.Mult_Damage = 1.25
