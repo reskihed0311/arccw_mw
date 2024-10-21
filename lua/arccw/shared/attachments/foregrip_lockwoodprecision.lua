117768232b29dcc0853f4745a093a3064df736e9
NotTrashraD <bellie1579@gmail.com>
2024-10-21 22:40:30 +0200

    add new atts

---
+++ lua/arccw/shared/attachments/foregrip_lockwoodprecision.lua
@@ -0,0 +1,22 @@
+att.PrintName = "LOCKGRIP Precision-40" 
+att.Description = "A classic vertical grip that helps you control recoil."
+
+att.SortOrder = 10
+
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "grip_mw"
+
+att.LHIK = true
+
+
+att.Model = "models/atts/ub/att_grip_lockgripprecision.mdl"
+
+
+att.Mult_SightTime = 1.20
+att.Mult_SpeedMult = 1
+att.Mult_Recoil = 0.75
+att.Mult_SightedSpeedMult = 0.90
---
+++ lua/arccw/shared/attachments/muz_broadhead.lua
@@ -0,0 +1,24 @@
+att.PrintName = "BROADHEAD-3DP"
+att.Description = "A low profile suppresor."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "muzzle"
+
+att.SortOrder = 0.5
+
+att.Model = "models/atts/muz/att_muz_broadhead3dp.mdl"
+
+att.Silencer = true
+att.Override_MuzzleEffect = "muzzleflash_suppressed"
+att.IsMuzzleDevice = false
+
+att.Mult_ShootPitch = 1
+att.Mult_ShootVol = 0.70
+att.Mult_AccuracyMOA = 0.95
+att.Mult_Range = 1
+att.Mult_Recoil = 0.95
+
+att.Mult_SightTime = 1.05
