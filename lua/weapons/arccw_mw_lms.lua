9f5c8935ea4d4959ef662266f83c42738e69f319
NotTrashraD <bellie1579@gmail.com>
2024-10-27 20:52:01 +0100

    LMS

---
+++ lua/weapons/arccw_mw_lms.lua
@@ -0,0 +1,248 @@
+AddCSLuaFile()
+
+
+SWEP.NeverPhysBullet = true
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW : Call Of Duty" -- edit this if you like
+SWEP.AdminOnly = false
+SWEP.Base = "arccw_base"
+SWEP.ViewModelFOV = 65
+SWEP.SubCategory = "Marksman Rifles"
+
+SWEP.PrintName = "LM-S"
+SWEP.Trivia_Class = "Assault DMR" -- "Submachine Gun"
+SWEP.Trivia_Desc = "The DMR version of the Lachmann & Meer Platform." -- "Ubiquitous 9mm SMG. Created as a response to the need for a faster-firing and more reliable submachine gun than existing options at the time."
+SWEP.Trivia_Manufacturer = "Lachmann & Meer" -- "Auschen Waffenfabrik"
+SWEP.Trivia_Calibre = "7.62x51MM" -- "9x21mm Jager"
+SWEP.Trivia_Mechanism = nil -- "Roller-Delayed Blowback"
+SWEP.Trivia_Country = "Germany" -- "Austria"
+SWEP.Trivia_Year = nil -- 1968
+
+
+SWEP.Slot = 2
+
+SWEP.UseHands = true
+
+
+SWEP.ViewModel = "models/weapons/arccw_mwr_lms.mdl" -- I mean, you probably have to edit these too
+SWEP.WorldModel = "models/weapons/w_rif_m4a1.mdl"
+
+SWEP.MirrorVMWM = true -- Copy the viewmodel, along with all its attachments, to the worldmodel. Super convenient!
+SWEP.MirrorWorldModel = "models/weapons/arccw_mwr_lms.mdl" -- Use this to set the mirrored viewmodel to a different model, without any floating speedloaders or cartridges you may have. Needs MirrorVMWM
+
+
+SWEP.WorldModelOffset =
+{
+    pos = Vector(-8.2, 4, -6.5),
+    ang = Angle(175, 180, 0),
+    scale = 0.95,
+}
+
+SWEP.Damage = 90
+SWEP.DamageMin = 60 -- damage done at maximum range
+
+SWEP.Range = 800 -- in METRES
+SWEP.Penetration = 10
+SWEP.DamageType = DMG_BULLET
+
+SWEP.TracerNum = 1 -- tracer every X
+SWEP.TracerFinalMag = 0 -- the last X bullets in a magazine are all tracers
+SWEP.Tracer = "arccw_tracer" -- override tracer (hitscan) effect
+SWEP.TracerCol = Color(255, 255, 255)
+
+
+SWEP.ChamberSize = 1 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 10  -- DefaultClip is automatically set.
+
+
+
+SWEP.Recoil = 1.8
+SWEP.RecoilSide = 0.3
+SWEP.RecoilRise = 0.95
+SWEP.MaxRecoilBlowback = -1
+SWEP.VisualRecoilMult = 0
+SWEP.RecoilPunch = 0.5
+SWEP.RecoilPunchBackMax = 1
+SWEP.RecoilPunchBackMaxSights = nil -- may clip with scopes
+SWEP.RecoilVMShake = 0.2-- random viewmodel offset when shooty
+
+
+SWEP.Sway = 0.35
+
+
+SWEP.Delay = 60 / 350-- 60 / RPM.
+
+
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0,
+    }
+
+}
+
+SWEP.AccuracyMOA = 2 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 450 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 150 -- inaccuracy added by moving. Applies in sights as well! Walking speed is considered as "maximum".
+SWEP.SightsDispersion = 0 -- dispersion that remains even in sights
+SWEP.JumpDispersion = 300 -- dispersion penalty when in the air
+
+
+SWEP.Primary.Ammo = "ar2" -- what ammo type the gun uses
+
+
+SWEP.ShootVol = 125 -- volume of shoot sound
+SWEP.ShootPitch = 100 -- pitch of shoot sound
+SWEP.ShootPitchVariation = 0.05
+
+SWEP.ShootSound = "weapons/lachmann762platform/fire_fp.wav"
+SWEP.ShootSoundSilenced = "weapons/lachmann762platform/fire_sil.wav"
+
+
+SWEP.MuzzleEffect = "muzzleflash_6"
+
+SWEP.ShellModel = "models/shells/shell_556.mdl"
+SWEP.ShellScale = 1.2
+SWEP.ShellPhysScale = 1
+SWEP.ShellPitch = 90
+SWEP.ShellSounds = "autocheck"--ArcCW.ShellSoundsTable
+SWEP.ShellRotate = 0
+SWEP.ShellTime = 0.5
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+SWEP.ProceduralViewBobAttachment = nil -- attachment on which coolview is affected by, default is muzzleeffect
+SWEP.CamAttachment = nil -- if set, this attachment will control camera movement
+SWEP.MuzzleFlashColor = Color(244, 209, 66)
+
+SWEP.SpeedMult = 0.85
+SWEP.SightedSpeedMult = 0.75
+SWEP.ShootSpeedMult = 0.70
+
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-3.961, -3.804, 1.84),
+    Ang = Angle(0, 0, 0),
+}
+
+
+SWEP.SightTime = 0.30
+
+
+SWEP.ActivePos = Vector(0, 0, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "ar2"
+SWEP.HoldtypeSights = "ar2"
+SWEP.HoldtypeCustomize = "slam"
+SWEP.HoldtypeSprintShoot = nil
+SWEP.HoldtypeNPC = nil
+
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+        },
+    ["holster"] = {
+        Source = "holster",
+        },
+    ["draw"] = {
+        Source = "draw",
+        },
+    ["fire"] = {
+           Source = "fire",
+           ShellEjectAt = 0,
+           },
+    ["fire_iron"] = {
+           Source = "fire_iron",
+           ShellEjectAt = 0,
+           },
+    ["fire_iron_empty"] = {
+           Source = "fire",
+           ShellEjectAt = 0,
+           },
+    ["reload_empty"] = {
+           Source = "reload_empty",
+           TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2, -- third person animation to play when this animation is played
+           LHIKIn = 0.15, -- In/Out controls how long it takes to switch to regular animation.
+       LHIKOut = 0.15, -- (not actually inverse kinematics)
+       LHIKEaseOut = 0.1, -- how long LHIK eases in.
+       LHIKEaseIn = 0.1, -- how long LHIK eases in.
+           LHIK = true, -- basically disable foregrips on this anim
+           SoundTable = {	
+		    {s = "weapons/lachmann762platform/raise_up.wav", t = 0},
+            {s = "weapons/lachmann762platform/lockback.wav" , t = 0.50},
+			{s = "weapons/lachmann762platform/smagout.wav" , t = 1.25},
+			{s = "weapons/lachmann762platform/smagin.wav" , t = 2.4},
+			{s = "weapons/lachmann762platform/release.wav" , t = 3.2},
+					{s = "weapons/lachmann762platform/shoulder.wav", t = 3.5},
+           },
+    },
+    ["reload"] = {
+       Source = "reload",
+       TPAnim = ACT_HL2MP_GESTURE_RELOAD_AR2, -- third person animation to play when this animation is played
+       LHIK = true, -- basically disable foregrips on this anim
+       LHIKIn = 0.15, -- In/Out controls how long it takes to switch to regular animation.
+       LHIKOut = 0.15, -- (not actually inverse kinematics)
+       LHIKEaseOut = 0.1, -- how long LHIK eases in.
+       LHIKEaseIn = 0.1, -- how long LHIK eases in.
+       SoundTable = {	
+	   {s = "weapons/lachmann762platform/raise_up.wav", t = 0},
+        {s = "weapons/lachmann762platform/smagout.wav" , t = 0.3},
+		{s = "weapons/lachmann762platform/smagin.wav" , t = 1.68},
+		{s = "weapons/lachmann762platform/shoulder.wav", t = 2},
+       },
+   },
+   }
+
+
+SWEP.DefaultBodygroups = "00000000000000000000000"
+
+
+SWEP.AttachmentElements = {
+    ["rail"] = {
+        VMBodygroups = {{ind = 1 , bg = 1}},
+    },
+}
+
+
+
+
+
+   SWEP.Attachments = {
+    {
+        PrintName = "Optic", -- print name
+        DefaultAttName = "Iron Sights",
+        Slot = {"optic" , "optic_sniper"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "weapon", -- relevant bone any attachments will be mostly referring to
+		InstalledEles = {"rail"},
+        Offset = {
+            vpos = Vector(0, -3.7, 1.5), -- offset that the attachment will be relative to the bone
+            vang = Angle(90, 0, -90),
+        },
+    },
+    {
+        PrintName = "Foregrip Slot", -- print name
+        DefaultAttName = "Your hand",
+        Slot = {"grip_mw", "bipod_mw"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "weapon", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(-1.85, -3.5, -7), -- offset that the attachment will be relative to the bone
+            vang = Angle(90, 0, -90),
+        },
+    },
+    {
+        PrintName = "Muzzle Device", -- print name
+        DefaultAttName = "Default Flashider",
+        Slot = {"muzzle"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "weapon", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(0, -1.85, 28), -- offset that the attachment will be relative to the bone
+            vang = Angle(-90, 0 ,-90),
+        },
+    },
+}
